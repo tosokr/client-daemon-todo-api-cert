4bbc615c3441e3831d6db8c90f4e2d473bd6c17d
Aleksandar Stefanov <aleksandar.stefanov@devoteam.nl>
2020-04-27 12:46:42 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates; //Only import this if you are using certificate
+using System.Threading.Tasks;
+
+namespace daemon_console
+{
+    /// <summary>
+    /// This sample shows how to query the Microsoft Graph from a daemon application
+    /// which uses application permissions.
+    /// For more information see https://aka.ms/msal-net-client-credentials
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RunAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+            var cert = new X509Certificate2( config.CertificateFileName, config.CertificatePassword);
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(cert);
+            var httpClient = new HttpClient(handler);
+            var apiCaller = new ProtectedApiCallHelper(httpClient);
+            await apiCaller.CallWebApiAndProcessResultASync($"{config.TodoListBaseAddress}/api/todolist", Display);
+
+         
+        }
+
+        /// <summary>
+        /// Display the result of the Web API call
+        /// </summary>
+        /// <param name="result">Object to display</param>
+        private static void Display(IEnumerable<JObject> result)
+        {
+            Console.WriteLine("Web Api result: \n");
+
+            foreach (var item in result)
+            {
+                foreach (JProperty child in item.Properties().Where(p => !p.Name.StartsWith("@")))
+                {
+                    Console.WriteLine($"{child.Name} = {child.Value}");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        /// <summary>
+        /// Checks if the sample is configured for using ClientSecret or Certificate. This method is just for the sake of this sample.
+        /// You won't need this verification in your production application since you will be authenticating in AAD using one mechanism only.
+        /// </summary>
+        /// <param name="config">Configuration from appsettings.json</param>
+        /// <returns></returns>
+
+    }
+}
